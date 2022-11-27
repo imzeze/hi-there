931ee39491554366b259b4717d58f6c24ba30a83
imzeze <jihyekim019@gmail.com>
2022-11-27 18:43:02 +0900

    feat: sort error 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import { Global } from '@emotion/react';
+
 import { Div } from '@/atoms';
 import globalStyle from '@/globalStyle';
-import { Global } from '@emotion/react';
 
 function App() {
   return (
     <Div>
       <Global styles={globalStyle} />
-      <Div width="100%" height="48px" backgroundColor="var(--paleGreen)">
-        HOME
+      <Div width="100%" height="60px" backgroundColor="var(--paleGreen)">
+        nav
       </Div>
     </Div>
   );
